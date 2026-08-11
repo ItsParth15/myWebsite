5eeb75bbc34cc766db8ea615cb200e9f8accd050
ItsParth15 <parthambavane67@gmail.com>
2026-08-11 23:07:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function scrollToWork() {
+    document.getElementById("work").scrollIntoView();
+}
+
+function contactMe() {
+    alert("Thanks for reaching out! Contact details on contact page.");
+}
